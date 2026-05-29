73da313c75f2c3293dcd0acb4829573ed5212692
Shaurya2006-hash <Shauryaaggnoida1@gmail.com>
2026-05-29 15:51:02 +0530

    Day 11 Task

--- Day-11/Backend 4/server.js
+++ Day-11/Backend 4/server.js
@@ -7,11 +7,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// routes
+// ==========================================
+// ROOT ROUTE
+// ==========================================
+app.get("/", (req, res) => {
+
+  res.send(
+    "VoiceNova AI Backend Running Successfully"
+  );
+
+});
+
+// ==========================================
+// API ROUTES
+// ==========================================
 app.use("/api", transcriptionRoutes);
 
-const PORT = 5000;
+// ==========================================
+// PORT
+// ==========================================
+const PORT =
+  process.env.PORT || 5000;
 
+// ==========================================
+// SERVER START
+// ==========================================
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+  console.log(
+    `Server running on port ${PORT}`
+  );
+
 });
